3b6088275e25d9ba7ff8f4e1d17e407ccc1825db
HP <Thenojan0310@gmail.com>
2020-04-29 21:39:50 +0530

    Rentdb

--- app/src/main/java/com/example/orbitz/RentDb.java
+++ app/src/main/java/com/example/orbitz/RentDb.java
@@ -10,4 +10,35 @@ public class RentDb {
     public RentDb() {
     }
 
-   
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCat() {
+        return cat;
+    }
+
+    public void setCat(String cat) {
+        this.cat = cat;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+}
