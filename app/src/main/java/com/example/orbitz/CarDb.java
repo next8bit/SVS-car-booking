061db3c3b2e54f75b85d7bd7a41b4b58685efc62
HP <Thenojan0310@gmail.com>
2020-04-29 21:38:09 +0530

    Cardb

--- app/src/main/java/com/example/orbitz/CarDb.java
+++ app/src/main/java/com/example/orbitz/CarDb.java
@@ -17,4 +17,19 @@ public class CarDb {
         this.manu = manu;
     }
 
-   
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getRegNo() {
+        return regNo;
+    }
+
+    public void setRegNo(String regNo) {
+        this.regNo = regNo;
+    }
+}
