1b5146da7bb005db7165280f8db0fac84970a6d0
HP <Thenojan0310@gmail.com>
2020-04-29 21:50:43 +0530

    carDetailsedit

--- app/src/main/java/com/example/orbitz/CarDetails.java
+++ app/src/main/java/com/example/orbitz/CarDetails.java
@@ -63,45 +63,7 @@ public class CarDetails extends AppCompatActivity {
         });
     }
 
-    public void AddData(){
-        savebt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                //boolean isInserted = c1.insertData(
-                dbref = FirebaseDatabase.getInstance().getReference().child("CarDb");
-            if(TextUtils.isEmpty(editman.getText().toString()))
-                Toast.makeText(getApplicationContext(), "Please enter a manufacturer name", Toast.LENGTH_LONG).show();
-            else if(TextUtils.isEmpty(editmod.getText().toString()))
-                Toast.makeText(getApplicationContext(), "Please enter a model name", Toast.LENGTH_LONG).show();
-            else if(TextUtils.isEmpty(editreg.getText().toString()))
-                Toast.makeText(getApplicationContext(), "Please a registration no", Toast.LENGTH_LONG).show();
-
-
-            else {
-
-                cardb.setManu(editman.getText().toString().trim());
-
-                cardb.setModel(editmod.getText().toString().trim());
-
-                cardb.setRegNo(editreg.getText().toString().trim());
-
-
-                dbref.push().setValue(cardb);
-
-                dbref.child("cardb1").setValue(cardb);
-
-                Toast.makeText(getApplicationContext(), "Saved Successfully", Toast.LENGTH_LONG).show();
-
-
-                editman.setText(null);
-
-                editmod.setText(null);
-
-                editreg.setText(null);
-            }
-
-
+    
 
 
 
