dc4c616729340446d48fc210afb2677b3cbf6003
sifran <sifranmohamed1996@gmail.com>
2020-04-30 12:39:01 +0530

    edit database

--- app/src/main/java/com/example/orbitz/CarDb.java
+++ app/src/main/java/com/example/orbitz/CarDb.java
@@ -21,4 +21,15 @@ public class CarDb {
         return model;
     }
 
-    
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getRegNo() {
+        return regNo;
+    }
+
+    public void setRegNo(String regNo) {
+        this.regNo = regNo;
+    }
+}
