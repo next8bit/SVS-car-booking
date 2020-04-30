97645baaa20b86fbe7d2abd54e8706a944fe24ba
ThushanthSbm <56014745+ThushanthSbm@users.noreply.github.com>
2020-04-30 19:31:40 +0530

    login edit7

--- app/src/main/java/com/example/orbitz/LoginDatabaseHelper.java
+++ app/src/main/java/com/example/orbitz/LoginDatabaseHelper.java
@@ -7,18 +7,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 
-public class LoginDatabaseHelper extends SQLiteOpenHelper {
-    public static final String DATABASE_NAME = "login.db";
 
-
-    public LoginDatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, 1);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE user(ID INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)");
-    }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
