8a34c8ce9d716865d4fae1042fe24b0b4060f37c
sifran <sifranmohamed1996@gmail.com>
2020-04-30 12:05:19 +0530

    edit car detials

--- app/src/main/java/com/example/orbitz/CarDetails.java
+++ app/src/main/java/com/example/orbitz/CarDetails.java
@@ -17,7 +17,10 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class CarDetails extends AppCompatActivity {
 
-    
+    Button savebt,typeb,nextb;
+    EditText editman,editmod,editreg;
+    DatabaseReference dbref;
+    CarDb car
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
