5ccb74d8c8f158d64f3458ced01a10351db9ed16
ThushanthSbm <56014745+ThushanthSbm@users.noreply.github.com>
2020-04-30 19:36:49 +0530

    login edit8

--- app/src/main/java/com/example/orbitz/SignUp.java
+++ app/src/main/java/com/example/orbitz/SignUp.java
@@ -13,7 +13,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 
+public class SignUp extends AppCompatActivity {
 
+    Button signupbtn,btnBack;
     EditText txtUN,txtEmail,txtPass,txtConPass;
     CheckBox terms;
     TextView usernameLbl,usernameLbl2;
