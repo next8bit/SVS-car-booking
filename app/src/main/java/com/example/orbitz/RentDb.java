d7f5e8ceac0dc4553485a132b04cc9ec3890cd06
sifran <sifranmohamed1996@gmail.com>
2020-04-30 21:30:22 +0530

    edit rent db

--- app/src/main/java/com/example/orbitz/RentDb.java
+++ app/src/main/java/com/example/orbitz/RentDb.java
@@ -13,32 +13,3 @@ public class RentDb {
     public String getId() {
         return id;
     }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getCat() {
-        return cat;
-    }
-
-    public void setCat(String cat) {
-        this.cat = cat;
-    }
-
-    public int getQty() {
-        return qty;
-    }
-
-    public void setQty(int qty) {
-        this.qty = qty;
-    }
-}
